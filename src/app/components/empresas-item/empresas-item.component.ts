6b5b4f0f76202ea6d3d65f14c6c16034149544d2
Cesar <reuscesar9@gmail.com>
2021-01-10 18:39:05 -0300

    Componente Listado de Empresas + Base de Componente Item Empresa

--- src/app/components/empresas-item/empresas-item.component.ts
+++ src/app/components/empresas-item/empresas-item.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Sale from 'src/app/models/sale.model';
 
 @Component({
-  selector: 'app-empresas-item',
+  selector: 'tr [app-empresas-item]',
   templateUrl: './empresas-item.component.html',
   styleUrls: ['./empresas-item.component.css']
 })
 export class EmpresasItemComponent implements OnInit {
 
+  @Input() empresa?: string;
+  @Input() sales?: Sale[];
+
+  comision: number = 0;
+  total?: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.total = this.sales.filter(sale => sale.nameAgency === this.empresa).reduce((a,b) => a + b.finalPrice, 0);
+    this.comision = this.total * 0.025;
   }
 
 }
