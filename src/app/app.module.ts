fc7835bc2bdc49b6dc2c903031a124ce8a8d781e
Cesar <reuscesar9@gmail.com>
2021-01-10 15:49:43 -0300

    Base service/components/model files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpresasListComponent } from './components/empresas-list/empresas-list.component';
+import { EmpresaDetailsComponent } from './components/empresa-details/empresa-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpresasListComponent,
+    EmpresaDetailsComponent
   ],
   imports: [
     BrowserModule,
